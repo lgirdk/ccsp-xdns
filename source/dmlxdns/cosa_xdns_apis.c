bc9490abccb2e1b514948c43e076a0312ddcdd5b
Shubham Baheti <shubham_baheti@comcast.com>
2016-12-12 20:36:41 -0800

    RDKB-9403 : Sites not getting blocked by Default IP setting
    Reason for change:  Default Device IP not blocking
    
    Test Procedure:     Boot up the box. Set Default IP to Level 3.
                        See that you can not browse listed sites.
    
    Change-Id: Ib3c651d3e85e1ee32d7e862aed7253c16123537b
    Risks: None
    Signed-off-by: Shubham_Baheti@cable.comcast.com

--- source/dmlxdns/cosa_xdns_apis.c
+++ source/dmlxdns/cosa_xdns_apis.c
@@ -61,6 +61,15 @@
 **************************************************************************/
 #include "plugin_main_apis.h"
 #include "cosa_xdns_apis.h"
+#include <sys/inotify.h>
+#include <limits.h>
+#include "ccsp_xdnsLog_wrapper.h"
+
+static pthread_mutex_t dnsmasqMutex = PTHREAD_MUTEX_INITIALIZER;
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+
+void* MonitorResolvConfForChanges(void *arg);
 
 
 void GetDnsMasqFileEntry(char* macaddress, char* defaultEntry)
@@ -73,12 +82,13 @@ void GetDnsMasqFileEntry(char* macaddress, char* defaultEntry)
         return;
     }
 
+    pthread_mutex_lock(&dnsmasqMutex);
 
     fp1 = fopen(DNSMASQ_SERVERS_CONF,"r");
 
     if(fp1 == NULL)
     {
-        printf("\nError reading file\n");
+        fprintf(stderr,"\nError reading file\n");
         return;
     }
     //Step 2: Get text from original file//
@@ -93,11 +103,157 @@ void GetDnsMasqFileEntry(char* macaddress, char* defaultEntry)
     }
 
     fclose(fp1);
+
+    pthread_mutex_unlock(&dnsmasqMutex);
+
 }
+
+void RefreshDnsmasqConfEntry()
+{
+    pthread_mutex_lock(&dnsmasqMutex);
+    char dnsmasqConfEntry[256] = {0};
+    char resolvConfEntry[256] = {0};
+
+    unlink(DNSMASQ_SERVERS_BAK);
+    //Step 1: Open text files and check that they open//
+    FILE *fp1 = NULL, *fp2 = NULL, *fp3 = NULL;
+
+    fp1 = fopen(RESOLV_CONF,"r");
+    fp2 = fopen(DNSMASQ_SERVERS_BAK ,"a");
+    fp3 = fopen(DNSMASQ_SERVERS_CONF,"r");
+
+    if(fp1 == NULL || fp2 == NULL || fp3 == NULL)
+    {
+        fprintf(stderr,"\nError reading file\n");
+        return;
+    }
+    //Step 2: Get text from original file//
+
+    while(fgets(resolvConfEntry, sizeof(resolvConfEntry), fp1) !=NULL)
+    {
+
+        if(strstr(resolvConfEntry, ":") != NULL)
+            {
+            continue;
+            }
+
+        fprintf(fp2, "%s", resolvConfEntry);
+    }
+
+    while(fgets(dnsmasqConfEntry, sizeof(dnsmasqConfEntry), fp3) !=NULL)
+    {
+        if ( !strstr(dnsmasqConfEntry, "dnsoverride"))
+        {
+            continue;
+        }
+
+        fprintf(fp2, "%s", dnsmasqConfEntry);
+    }
+
+    fclose(fp1);
+    fclose(fp3);
+
+    unlink(DNSMASQ_SERVERS_CONF);
+    fclose(fp2);
+    rename(DNSMASQ_SERVERS_BAK, DNSMASQ_SERVERS_CONF);
+
+    pthread_mutex_unlock(&dnsmasqMutex);
+
+}
+
+
+ static void ResolvConfFileEvent(struct inotify_event *i)
+ {
+     fprintf(stderr, "    wd =%2d; ", i->wd);
+     fprintf(stderr, "mask = ");
+
+     if (i->mask & IN_ACCESS)        fprintf(stderr, "IN_ACCESS ");
+     if (i->mask & IN_ATTRIB)        fprintf(stderr, "IN_ATTRIB ");
+     if (i->mask & IN_CLOSE_NOWRITE) fprintf(stderr, "IN_CLOSE_NOWRITE ");
+     if (i->mask & IN_CLOSE_WRITE)   fprintf(stderr, "IN_CLOSE_WRITE ");
+     if (i->mask & IN_CREATE)        fprintf(stderr, "IN_CREATE ");
+     if (i->mask & IN_DELETE)        fprintf(stderr, "IN_DELETE ");
+     if (i->mask & IN_DELETE_SELF)   fprintf(stderr, "IN_DELETE_SELF ");
+     if (i->mask & IN_IGNORED)       fprintf(stderr, "IN_IGNORED ");
+     if (i->mask & IN_ISDIR)         fprintf(stderr, "IN_ISDIR ");
+     if (i->mask & IN_MODIFY)        fprintf(stderr, "IN_MODIFY ");
+     if (i->mask & IN_MOVE_SELF)     fprintf(stderr, "IN_MOVE_SELF ");
+     if (i->mask & IN_MOVED_FROM)    fprintf(stderr, "IN_MOVED_FROM ");
+     if (i->mask & IN_MOVED_TO)      fprintf(stderr, "IN_MOVED_TO ");
+     if (i->mask & IN_OPEN)          fprintf(stderr, "IN_OPEN ");
+     if (i->mask & IN_Q_OVERFLOW)    fprintf(stderr, "IN_Q_OVERFLOW ");
+     if (i->mask & IN_UNMOUNT)       fprintf(stderr, "IN_UNMOUNT ");
+
+     if (i->mask & IN_CLOSE_WRITE || i->mask & IN_ATTRIB)
+     {
+        fprintf(stderr, "IN_CLOSE_WRITE || IN_ATTRIB");
+        sleep(5);
+        RefreshDnsmasqConfEntry();
+     } 
+
+     fprintf(stderr, "\n");
+ 
+     if (i->len > 0)
+         fprintf(stderr, "        name = %s\n", i->name);
+ }
+ 
+void* MonitorResolvConfForChanges(void *arg)
+{
+    int inotifyFd, wd, j;
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    ssize_t numRead;
+    char *p;
+    struct inotify_event *event;
+
+    inotifyFd = inotify_init();                 /* Create inotify instance */
+    if (inotifyFd == -1)
+        {
+            CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : inotify_init error  \n", __FUNCTION__ ));
+            return;
+        }
+
+     wd = inotify_add_watch(inotifyFd, RESOLV_CONF, IN_ALL_EVENTS);
+     if (wd == -1)
+        {
+            CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : inotify_add_watch error  \n", __FUNCTION__ ));
+            return;
+        }
+
+    CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : Watching RESOLV_CONF using wd %d  \n", __FUNCTION__ , wd));
+
+    for (;;) 
+    {                                  /* Read events forever */
+     numRead = read(inotifyFd, buf, BUF_LEN);
+     if (numRead == 0)
+        {
+            CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : read() from inotify fd returned  numRead %d  \n", __FUNCTION__ , numRead));
+        }
+
+     if (numRead == -1)
+        {
+            CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : read error numRead %d  \n", __FUNCTION__ , numRead));
+        }
+
+    CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : Read %ld bytes from inotify fd\n \n", __FUNCTION__ , numRead));
+
+     /* Process all of the events in buffer returned by read() */
+
+     for (p = buf; p < buf + numRead; ) 
+        {
+         event = (struct inotify_event *) p;
+         ResolvConfFileEvent(event);
+
+         p += sizeof(struct inotify_event) + event->len;
+        }
+    }
+}
+
 
 void ReplaceDnsmasqConfEntry(char* macaddress, char* overrideEntry)
 {
     char dnsmasqConfEntry[256] = {0};
+
+    pthread_mutex_lock(&dnsmasqMutex);
 
 
     if(!macaddress || !strlen(macaddress))
@@ -113,7 +269,7 @@ void ReplaceDnsmasqConfEntry(char* macaddress, char* overrideEntry)
 
     if(fp1 == NULL || fp2 == NULL)
     {
-        printf("\nError reading file\n");
+        fprintf(stderr,"\nError reading file\n");
         return;
     }
     //Step 2: Get text from original file//
@@ -134,21 +290,31 @@ void ReplaceDnsmasqConfEntry(char* macaddress, char* overrideEntry)
     unlink(DNSMASQ_SERVERS_CONF);
     fclose(fp2);
     rename(DNSMASQ_SERVERS_BAK, DNSMASQ_SERVERS_CONF);
+
+    pthread_mutex_unlock(&dnsmasqMutex);
+
 }
 
 void AppendDnsmasqConfEntry(char* string1)
 {
     FILE *fp2;
+
+    pthread_mutex_lock(&dnsmasqMutex);
+
+
     fp2 = fopen(DNSMASQ_SERVERS_CONF ,"a");
 
     if(fp2 == NULL)
     {
-        printf("\nError reading file\n");
+        fprintf(stderr,"\nError reading file\n");
         return;
     }
 
     fprintf(fp2, "%s", string1);
     fclose(fp2);
+
+    pthread_mutex_unlock(&dnsmasqMutex);
+
 }
 
 
@@ -171,11 +337,9 @@ void CreateDnsmasqServerConf(PCOSA_DATAMODEL_XDNS pMyObject)
     fp1 = fopen(RESOLV_CONF,"r");
     fp2 = fopen(DNSMASQ_SERVERS_CONF ,"w");
 
-
-
     if(fp1 == NULL || fp2 == NULL)
     {
-        printf("\nError reading file\n");
+        fprintf(stderr,"\nError reading file\n");
         return;
     }
     //Step 2: Get text from original file//
@@ -184,6 +348,11 @@ void CreateDnsmasqServerConf(PCOSA_DATAMODEL_XDNS pMyObject)
     	if(strstr(resolvConfEntry, "nameserver") != NULL)
     		{
                 strcpy(buf, resolvConfEntry);
+
+                if(strstr(resolvConfEntry, ":") != NULL)
+                {
+                    continue;
+                }
 
                 char *newline = strchr( buf, '\n' );
                 if ( newline )
@@ -270,112 +439,122 @@ CosaDmlGetSelfHealCfg(
 
 	pMappingContainer = (PCOSA_DML_MAPPING_CONTAINER)AnscAllocateMemory(sizeof(COSA_DML_MAPPING_CONTAINER));
 
-    if ( (fp_dnsmasq_conf=fopen(DNSMASQ_SERVERS_CONF, "r")) == NULL )
+    if( access( DNSMASQ_SERVERS_CONF, F_OK ) != -1 )
     {
-    	pMappingContainer->XDNSEntryCount = 0;
-    	CreateDnsmasqServerConf(pMyObject);
+        RefreshDnsmasqConfEntry();
+    } 
+    else 
+    {
+        pMappingContainer->XDNSEntryCount = 0;
+        CreateDnsmasqServerConf(pMyObject);
         return pMappingContainer;
     }
 
-    while ( fgets(buf, sizeof(buf), fp_dnsmasq_conf)!= NULL )
+    pthread_mutex_lock(&dnsmasqMutex);
+
+    if ( (fp_dnsmasq_conf=fopen(DNSMASQ_SERVERS_CONF, "r")) != NULL )
     {
-        char *newline = strchr( buf, '\n' );
-        if ( newline )
-            *newline = 0;
-
-        if ( !strstr(buf, "dnsoverride"))
+        while ( fgets(buf, sizeof(buf), fp_dnsmasq_conf)!= NULL )
         {
-            continue;
-        }
+            char *newline = strchr( buf, '\n' );
+            if ( newline )
+                *newline = 0;
 
-
-        if (strstr(buf, "00:00:00:00:00:00"))
-        {
-            char* token = NULL;
-            const char* s = " ";
-            token = strtok(buf, s);
-            if(!token)
-            {
-                continue;   
-            }   
-            
-            token = strtok(NULL, s);
-            if(!token)
-            {
-                continue;   
-            } 
-
-            token = strtok(NULL, s);
-            if(token && strstr(token, "."))
-            {
-                strcpy(pMyObject->DefaultDeviceDnsIPv4, token);
-            }
-            else
+            if ( !strstr(buf, "dnsoverride"))
             {
                 continue;
             }
 
-#ifdef FEATURE_IPV6
-            token = strtok(NULL, s);
-            if(token && strstr(token, ":"))
+
+            if (strstr(buf, "00:00:00:00:00:00"))
             {
-                strcpy(pMyObject->DefaultDeviceDnsIPv6, token);
-            }
-            else
-            {
+                char* token = NULL;
+                const char* s = " ";
+                token = strtok(buf, s);
+                if(!token)
+                {
+                    continue;   
+                }   
+                
+                token = strtok(NULL, s);
+                if(!token)
+                {
+                    continue;   
+                } 
+
+                token = strtok(NULL, s);
+                if(token && strstr(token, "."))
+                {
+                    strcpy(pMyObject->DefaultDeviceDnsIPv4, token);
+                }
+                else
+                {
+                    continue;
+                }
+
+    #ifdef FEATURE_IPV6
+                token = strtok(NULL, s);
+                if(token && strstr(token, ":"))
+                {
+                    strcpy(pMyObject->DefaultDeviceDnsIPv6, token);
+                }
+                else
+                {
+                    continue;
+                }
+    #endif
+
+                token = strtok(NULL, s);
+                if(token)
+                {
+                    strcpy(pMyObject->DefaultDeviceTag, token);
+                }
+
                 continue;
             }
-#endif
 
-            token = strtok(NULL, s);
-            if(token)
+            pDnsTableEntry = (PCOSA_DML_XDNS_MACDNS_MAPPING_ENTRY)AnscAllocateMemory(sizeof(COSA_DML_XDNS_MACDNS_MAPPING_ENTRY));
+            if ( !pDnsTableEntry )
             {
-                strcpy(pMyObject->DefaultDeviceTag, token);
+                CcspTraceWarning(("%s resource allocation failed\n",__FUNCTION__));
+                fclose(fp_dnsmasq_conf);
+                unlink(DNSMASQ_SERVERS_CONF);
+                break;
             }
 
-            continue;
+            /*
+            Sample:
+            dnsoverride AA:BB:CC:DD:EE:FF 1.2.3.4 ArbitrarySting
+            */
+
+            ret = sscanf(buf, XDNS_ENTRY_FORMAT,
+                     stub,
+                     pDnsTableEntry->MacAddress,
+                     pDnsTableEntry->DnsIPv4,
+    #ifdef FEATURE_IPV6
+                     pDnsTableEntry->DnsIPv6,
+    #endif                 
+                     pDnsTableEntry->Tag);
+
+    /*        fprintf(stderr, "%s:%s %s %s\n", __FUNCTION__,
+                    pDnsTableEntry->MacAddress,
+                    pDnsTableEntry->DnsIPv4,
+                    pDnsTableEntry->Tag);
+    */
+
+
+            if(ret < 3)
+            {
+                free(pDnsTableEntry);
+                continue;
+            }
+
+            index++;        
+
+
+            FillEntryInList(pMyObject, pDnsTableEntry);
         }
 
-        pDnsTableEntry = (PCOSA_DML_XDNS_MACDNS_MAPPING_ENTRY)AnscAllocateMemory(sizeof(COSA_DML_XDNS_MACDNS_MAPPING_ENTRY));
-        if ( !pDnsTableEntry )
-        {
-            CcspTraceWarning(("%s resource allocation failed\n",__FUNCTION__));
-            fclose(fp_dnsmasq_conf);
-            unlink(DNSMASQ_SERVERS_CONF);
-            break;
-        }
-
-        /*
-        Sample:
-        dnsoverride AA:BB:CC:DD:EE:FF 1.2.3.4 ArbitrarySting
-        */
-
-        ret = sscanf(buf, XDNS_ENTRY_FORMAT,
-                 stub,
-                 pDnsTableEntry->MacAddress,
-                 pDnsTableEntry->DnsIPv4,
-#ifdef FEATURE_IPV6
-                 pDnsTableEntry->DnsIPv6,
-#endif                 
-                 pDnsTableEntry->Tag);
-
-/*        fprintf(stderr, "%s:%s %s %s\n", __FUNCTION__,
-                pDnsTableEntry->MacAddress,
-                pDnsTableEntry->DnsIPv4,
-                pDnsTableEntry->Tag);
-*/
-
-
-        if(ret != 4)
-        {
-            free(pDnsTableEntry);
-            continue;
-        }
-
-        index++;		
-
-
-		FillEntryInList(pMyObject, pDnsTableEntry);
     }
 
     pMappingContainer->XDNSEntryCount = index;
@@ -383,6 +562,8 @@ CosaDmlGetSelfHealCfg(
     //fprintf(stderr, "index %d\n", index);
 
     fclose(fp_dnsmasq_conf);
+
+    pthread_mutex_unlock(&dnsmasqMutex);
 
 	return pMappingContainer;
 }
@@ -473,13 +654,19 @@ CosaXDNSInitialize
     ANSC_STATUS                     returnStatus         = ANSC_STATUS_SUCCESS;
     PCOSA_DATAMODEL_XDNS            pMyObject            = (PCOSA_DATAMODEL_XDNS )hThisObject;
 	PCOSA_DML_MAPPING_CONTAINER    pMappingContainer            = (PCOSA_DML_MAPPING_CONTAINER)NULL;
-	
-    /* Initiation all functions */
+    pthread_t tid;
+
     AnscSListInitializeHeader( &pMyObject->XDNSDeviceList );
     pMyObject->MaxInstanceNumber        = 0;
     pMyObject->ulXDNSNextInstanceNumber   = 1;
 	
     pMyObject->pMappingContainer = CosaDmlGetSelfHealCfg((ANSC_HANDLE)pMyObject);
+
+    if (pthread_create(&tid, NULL, MonitorResolvConfForChanges, NULL))
+    {
+        CcspXdnsConsoleTrace(("RDK_LOG_ERROR, CcspXDNS %s : Failed to Start Thread to start MonitorResolvConfForChanges  \n", __FUNCTION__ ));
+        return ANSC_STATUS_FAILURE;
+    }
 
     return returnStatus;
 }
